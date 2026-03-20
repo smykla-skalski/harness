a589b015a9c6f1ff3993cba5d45100b2a9ca4ffd
Bart Smykla <bartek@smykla.com>
2026-03-20 15:56:49 +0100

    refactor(setup): split build info tests from production module
    Signed-off-by: Bart Smykla <bartek@smykla.com>

--- src/setup/build_info.rs
+++ src/setup/build_info.rs
@@ -72,15 +72,4 @@ pub fn resolve_build_info(repo: &Path) -> Result<BuildInfo, CliError> {
 }
 
 #[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn build_info_env() {
-        let info = BuildInfo {
-            version: "1.2.3".into(),
-        };
-        let env = info.env();
-        assert_eq!(env.get("BUILD_INFO_VERSION").unwrap(), "1.2.3");
-    }
-}
+mod tests;
