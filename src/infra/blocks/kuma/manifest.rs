56b7a8591162871129829c11198bea2e0e690c34
Bart Smykla <bartek@smykla.com>
2026-03-21 11:10:12 +0100

    refactor(kuma): split block root tests from production modules
    Signed-off-by: Bart Smykla <bartek@smykla.com>

--- src/infra/blocks/kuma/manifest.rs
+++ src/infra/blocks/kuma/manifest.rs
@@ -42,22 +42,5 @@ pub fn resource_api_path(
 }
 
 #[cfg(test)]
-mod tests {
-    use super::resource_api_path;
-
-    #[test]
-    fn mesh_resource_uses_top_level_path() {
-        assert_eq!(
-            resource_api_path("Mesh", "default", None).unwrap(),
-            "/meshes/default"
-        );
-    }
-
-    #[test]
-    fn mesh_scoped_resource_defaults_mesh() {
-        assert_eq!(
-            resource_api_path("MeshTimeout", "demo", None).unwrap(),
-            "/meshes/default/meshtimeouts/demo"
-        );
-    }
-}
+#[path = "manifest/tests.rs"]
+mod tests;
