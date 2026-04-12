80d547b7a68e176d403f16d62d3d9630ce24c13a
Bart Smykla <bartek@smykla.com>
2026-04-12 11:04:10 +0200

    Revert "perf(monitor): batch commands display state"
    This reverts commit 11c1eaef0ba7ddd495d30cb2cc45f5b9e5affb28.
    Signed-off-by: Bart Smykla <bartek@smykla.com>

--- apps/harness-monitor-macos/Sources/HarnessMonitorKit/Stores/HarnessMonitorStore+Slices.swift
+++ apps/harness-monitor-macos/Sources/HarnessMonitorKit/Stores/HarnessMonitorStore+Slices.swift
@@ -468,13 +468,37 @@ extension HarnessMonitorStore {
   @MainActor
   @Observable
   public final class CommandsUISlice {
-    public var displayState = CommandsUIState()
+    public var canNavigateBack = false
+    public var canNavigateForward = false
+    public var hasSelectedSession = false
+    public var isSessionReadOnly = true
+    public var bookmarkTitle = "Bookmark Session"
+    public var isPersistenceAvailable = false
+    public var hasObserver = false
 
     public init() {}
 
     internal func apply(_ state: CommandsUIState) {
-      if displayState != state {
-        displayState = state
+      if canNavigateBack != state.canNavigateBack {
+        canNavigateBack = state.canNavigateBack
+      }
+      if canNavigateForward != state.canNavigateForward {
+        canNavigateForward = state.canNavigateForward
+      }
+      if hasSelectedSession != state.hasSelectedSession {
+        hasSelectedSession = state.hasSelectedSession
+      }
+      if isSessionReadOnly != state.isSessionReadOnly {
+        isSessionReadOnly = state.isSessionReadOnly
+      }
+      if bookmarkTitle != state.bookmarkTitle {
+        bookmarkTitle = state.bookmarkTitle
+      }
+      if isPersistenceAvailable != state.isPersistenceAvailable {
+        isPersistenceAvailable = state.isPersistenceAvailable
+      }
+      if hasObserver != state.hasObserver {
+        hasObserver = state.hasObserver
       }
     }
   }
