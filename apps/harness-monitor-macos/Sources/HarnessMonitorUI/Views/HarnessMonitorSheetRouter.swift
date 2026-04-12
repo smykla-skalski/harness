ff7bb851a5d4664b1e82d4b7cb7f00ae37e494bd
Bart Smykla <bartek@smykla.com>
2026-04-12 08:55:10 +0200

    refactor(monitor): remove agentTui sheet
    Signed-off-by: Bart Smykla <bartek@smykla.com>

--- apps/harness-monitor-macos/Sources/HarnessMonitorUI/Views/HarnessMonitorSheetRouter.swift
+++ apps/harness-monitor-macos/Sources/HarnessMonitorUI/Views/HarnessMonitorSheetRouter.swift
@@ -12,8 +12,6 @@ private struct HarnessMonitorSheetMetrics {
       Self(minWidth: 420, idealWidth: 500, minHeight: 300)
     case .codexFlow:
       Self(minWidth: 520, idealWidth: 620, minHeight: 520)
-    case .agentTui:
-      Self(minWidth: 860, idealWidth: 980, minHeight: 620)
     }
   }
 }
@@ -34,8 +32,6 @@ struct HarnessMonitorSheetRouter: View {
   @ViewBuilder
   private var sheetContent: some View {
     switch sheet {
-    case .agentTui:
-      AgentTuiSheetView(store: store)
     case .codexFlow:
       CodexFlowSheetView(store: store)
     case .sendSignal(let agentID):
