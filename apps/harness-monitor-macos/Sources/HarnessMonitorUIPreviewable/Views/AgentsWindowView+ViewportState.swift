18336f055d78c959f0fcc7d4aaaf36051325ef9e
Bart Smykla <bartek@smykla.com>
2026-05-01 00:00:11 +0200

    perf(monitor): reduce workspace terminal refresh churn
    Signed-off-by: Bart Smykla <bartek@smykla.com>

---
+++ apps/harness-monitor-macos/Sources/HarnessMonitorUIPreviewable/Views/AgentsWindowView+ViewportState.swift
@@ -0,0 +1,39 @@
+import CoreGraphics
+import HarnessMonitorKit
+
+extension AgentsWindowView {
+  func recordMeasuredViewportPoints(_ viewportSize: CGSize) {
+    guard viewModel.lastMeasuredViewportPoints != viewportSize else {
+      return
+    }
+    viewModel.lastMeasuredViewportPoints = viewportSize
+  }
+
+  func clearMeasuredViewportTerminalSize() {
+    guard viewModel.lastMeasuredViewportTerminalSize != nil else {
+      return
+    }
+    viewModel.lastMeasuredViewportTerminalSize = nil
+  }
+
+  func recordMeasuredViewportTerminalSize(_ terminalSize: AgentTuiSize) {
+    guard viewModel.lastMeasuredViewportTerminalSize != terminalSize else {
+      return
+    }
+    viewModel.lastMeasuredViewportTerminalSize = terminalSize
+  }
+
+  func recordAutomaticViewportSize(_ terminalSize: AgentTuiSize) {
+    guard viewModel.lastMeasuredViewportSize != terminalSize else {
+      return
+    }
+    viewModel.lastMeasuredViewportSize = terminalSize
+  }
+
+  func recordExpectedViewportSize(_ terminalSize: AgentTuiSize) {
+    guard viewModel.expectedSize != terminalSize else {
+      return
+    }
+    viewModel.expectedSize = terminalSize
+  }
+}
