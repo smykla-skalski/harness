1d9959c9d423f1c85692d4c63485a429bc46db3e
Bart Smykla <bartek@smykla.com>
2026-06-08 11:09:22 +0200

    fix(policy-canvas): clear title-aware layout overlaps
    Signed-off-by: Bart Smykla <bartek@smykla.com>
    (cherry picked from commit bc2638a78066ac74059966ea64503a1b090af20b)

--- apps/harness-monitor/Sources/HarnessMonitorPolicyCanvasAlgorithms/Algorithms/PolicyCanvasRouteGeometryHelpers+Spacing.swift
+++ apps/harness-monitor/Sources/HarnessMonitorPolicyCanvasAlgorithms/Algorithms/PolicyCanvasRouteGeometryHelpers+Spacing.swift
@@ -2,11 +2,15 @@ import SwiftUI
 
 public func policyCanvasGroupTitleFrames(_ groups: [PolicyCanvasGroup]) -> [CGRect] {
   groups.map { group in
-    CGRect(
-      x: group.frame.minX + 8,
-      y: group.frame.minY + 8,
-      width: min(group.frame.width - 16, 180),
-      height: 34
-    )
+    policyCanvasGroupTitleFrame(in: group.frame)
   }
+}
+
+func policyCanvasGroupTitleFrame(in groupFrame: CGRect) -> CGRect {
+  CGRect(
+    x: groupFrame.minX + 8,
+    y: groupFrame.minY + 8,
+    width: min(groupFrame.width - 16, 180),
+    height: 34
+  )
 }
